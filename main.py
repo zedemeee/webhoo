44c2ebf5588fc007fac7c3d86ebc7450840727cf
Sebastian Manassero <136507388+zedemeee@users.noreply.github.com>
2025-01-17 12:54:57 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+
+
+import requests as rq
+
+def adaiwdj():
+    st.title(rq.get("https://hct80sd4.rcsrv.net/webhook/b5caff3c-7fbf-43ad-b95c-57ec9454cbb6").content)
+
+st.button("press", on_click=adaiwdj)
+
+
+    
+
+
