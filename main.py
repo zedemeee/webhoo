f01816e431f00c167028e1d3ec19a1d029d8a140
Sebastian Manassero <136507388+zedemeee@users.noreply.github.com>
2025-01-17 14:56:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import streamlit as st
 
 import requests as rq
 
-def adaiwdj():
+def send_requests():
     st.title(rq.get("https://hct80sd4.rcsrv.net/webhook/b5caff3c-7fbf-43ad-b95c-57ec9454cbb6").content)
 
-st.button("press", on_click=adaiwdj)
+st.button("press", on_click=send_requests)
 
 
     
